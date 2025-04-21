c04094f5973add5b7b48a48e72905c3fc0727a40
protodeleter <pavel.nsnv@gmail.com>
2025-04-21 13:48:49 +0300

    conditions

---
+++ conditions.js
@@ -0,0 +1,58 @@
+'use_strict';
+
+// if ("0") {
+//     alert("Hello, world!");
+// }
+
+
+
+// if (prompt("What is an actual name of javascript?") == "ECMAScript") {
+//     alert("Correct!");
+// } else {
+//     alert("You don't know? ECMAScript!");
+// }
+
+
+// let score = 75;
+// let grade;
+
+// grade = (score >= 90) ? "A" :
+//     (score >= 80) ? "B" :
+//         (score >= 70) ? "C" :
+//             (score >= 60) ? "D" : "F";
+
+// console.log("הציון הוא:", grade);
+
+
+// let num = prompt("Enter a number:");
+// let result = (num > 0) ? "Positive" :
+//     (num < 0) ? "Negative" : "Zero";
+
+// alert(result);
+
+
+// let result;
+// result = (a + b < 4) ? 'Not enough' : 'A lot';
+// console.log(result);
+
+
+// let message;
+
+// message = (login == 'Employee') ? 'Hello' :
+//     (login == 'Director') ? 'Hello' :
+//         (login == '') ? 'No login' : '';
+// console.log(message);
+
+console.log(false || "sdf"); // sdf
+console.log(false && ''); // false
+console.log(true || ''); // true
+console.log(undefined || null); // null
+console.log(undefined || 0 || null); // null
+console.log(undefined && 0 && null); // undefined
+console.log(1 || 0); // 1
+console.log(null || 1);
+console.log(null || 0 || 1); // 1
+
+
+
+
